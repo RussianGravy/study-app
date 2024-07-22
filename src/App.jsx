12a8e5cd30f73d11d5d8633a6f6e0645840d6c5b
RussianGravy <valentinejones1215@gmail.com>
2024-07-22 15:03:51 -0700

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,18 @@ import ReactDOM from "react-dom";
 import { LoginPage } from "./otherpages/LoginPage.jsx";
 import { HomePage } from "./otherpages/HomePage.jsx";
 import { auth, googleProvider, db } from "./config/firebase.js";
+//temp
+import { SignUp } from "./src/components/SignUp.jsx";
 
 function App() {
-  //   const loadPage = async () => {
-  //     if (auth?.currentUser?.email != undefined) return <HomePage></HomePage>;
-  //     else return <LoginPage></LoginPage>;
-  //   };
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LoginPage />} />
-        <Route path="/homepage" element={<HomePage />} />
+        <Route index element={<SignUp />} />
+        {/* <Route index element={<LoginPage />} />
+        <Route path="/homepage" element={<HomePage />} /> */}
       </Routes>
     </BrowserRouter>
-    // <>{loadPage}</>
   );
 }
 
