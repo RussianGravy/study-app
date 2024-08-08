070a1aca6c68f5d05c02fd19a7772250d4f7a8e0
RussianGravy <valentinejones1215@gmail.com>
2024-08-07 21:12:50 -0700

    added modal functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,17 @@ import { get } from "firebase/database";
 
 export function Card({ topic, content, deleteFunction, updateFunction }) {
   //Edit-menu variables and functions
-  const [toggleVariable, setToggleVariable] = useState(-1);
-  const toggleEditMenu = () => {
-    setToggleVariable(toggleVariable * -1);
-  };
-  const getStyles = () => {
-    if (toggleVariable > 0) {
-      return "m-0 p-0 bg-slate-300 w-full h-full absolute top-0 left-0 flex flex-col"; //show menu
-    } else {
-      return "opacity-0 pointer-events-none"; //hide menu
-    }
-  };
+  // const [toggleVariable, setToggleVariable] = useState(-1);
+  // const toggleEditMenu = () => {
+  //   setToggleVariable(toggleVariable * -1);
+  // };
+  // const getStyles = () => {
+  //   if (toggleVariable > 0) {
+  //     return "m-0 p-0 bg-slate-300 w-full h-full absolute top-0 left-0 flex flex-col"; //show menu
+  //   } else {
+  //     return "opacity-0 pointer-events-none"; //hide menu
+  //   }
+  // };
   return (
     <div className="bg-slate-300 flex flex-col mx-5 my-14 w-80 h-96 p-3 rounded-lg border-2 border-black relative overflow-hidden">
       <div className="text-5xl h-7 w-full self-center flex flex-row-reverse z-30">
@@ -27,14 +27,14 @@ export function Card({ topic, content, deleteFunction, updateFunction }) {
         </button>
         <button
           className="text-white text-xl w-7 h-7 mr-3 rounded-full bg-blue-600"
-          onClick={toggleEditMenu}
+          onClick={updateFunction}
         >
           E
         </button>
       </div>
       <h1 className=" text-5xl">{topic}</h1>
       <p className=" text-3xl text-gray-700">{content}</p>
-      <div className={getStyles()}>
+      {/* <div className={getStyles()}>
         <div className="mx-3 my-9 w-fit">
           <input
             type="text"
@@ -62,10 +62,7 @@ export function Card({ topic, content, deleteFunction, updateFunction }) {
         >
           Update
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
-
-export var updatedTopic;
-export var updatedContent;
--- src/otherpages/HomePage.jsx
+++ src/otherpages/HomePage.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { Navbar } from "../components/Navbar.jsx";
-import Auth from "../components/Auth.jsx";
-import { Card, updatedContent, updatedTopic } from "../components/Card.jsx";
+import { Card } from "../components/Card.jsx";
 import {
   CreateCardButton,
   newContent,
@@ -24,7 +22,9 @@ import Modal from "../components/Modal.jsx";
 
 export function HomePage() {
   const [cardList, setCardList] = useState([]);
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
+  const [modalContent, setModalContent] = useState(<></>);
+  const modalVariables = { id: null };
   const temp = useAuth();
   const cardsCollectionsRef = getCollection();
   function getCollection() {
@@ -60,24 +60,31 @@ export function HomePage() {
         content: newContent,
         userId: auth?.currentUser?.uid,
       });
-      getCardList();
+      await getCardList();
     } catch (err) {
       console.error(err);
     }
   };
 
   const deleteCard = async (id) => {
-    try {
-      const cardDoc = doc(db, temp.currentUser.email, id);
-      await deleteDoc(cardDoc);
-      getCardList();
-    } catch (err) {
-      console.error(err);
-    }
+    setModalContent(DeleteMenu);
+    console.log("delete button pressed, toggle set to " + toggle);
+    await setToggle(true);
+    modalVariables.id = id;
+    // try {
+    //   const cardDoc = doc(db, temp.currentUser.email, id);
+    //   await deleteDoc(cardDoc);
+    //   getCardList();
+    // } catch (err) {
+    //   console.error(err);
+    // }
   };
 
-  const updateCard = async (id, t, c) => {
-    setToggle(!toggle);
+  const updateCard = async (id) => {
+    setModalContent(EditMenu);
+    await setToggle(true);
+    console.log("update button pressed, toggle set to " + toggle);
+    modalVariables.id = id;
     // const cardDoc = doc(db, temp.currentUser.email, id);
     // try {
     //   await updateDoc(cardDoc, { title: t });
@@ -92,6 +99,97 @@ export function HomePage() {
     //   console.error(err);
     // }
   };
+
+  const DeleteMenu = (
+    <div className="bg-slate-300 flex flex-col mx-5 my-14 w-80 h-fit p-3 rounded-lg border-2 border-black relative overflow-hidden">
+      <div className="text-5xl h-7 w-full self-center flex z-30">
+        <p className="grow text-2xl">Deleting Card</p>
+        <button
+          onClick={async () => {
+            await setToggle(false);
+            console.log("toggled modal to " + toggle);
+            modalVariables.id = null;
+            modalVariables.topic = null;
+            modalVariables.content = null;
+          }}
+          className="text-white text-xl w-fit px-1 rounded bg-red-600"
+        >
+          Cancel
+        </button>
+      </div>
+      <div className="w-fit mx-auto my-8 text-xl">
+        Are you Sure?{" "}
+        <button
+          className="text-blue-600"
+          onClick={async () => {
+            const cardDoc = doc(db, temp.currentUser.email, modalVariables.id);
+            console.log(cardDoc);
+            deleteDoc(cardDoc);
+            await getCardList();
+            setToggle(false);
+          }}
+        >
+          Yes
+        </button>
+      </div>
+    </div>
+  );
+
+  var updatedTopic;
+  var updatedContent;
+  const EditMenu = (
+    <div className="bg-slate-300 flex flex-col mx-5 my-14 w-80 h-fit p-3 rounded-lg border-2 border-black relative overflow-hidden">
+      <div className="text-5xl h-7 w-full self-center flex z-30">
+        <p className="grow text-2xl">Updating Card</p>
+        <button
+          onClick={async () => {
+            await setToggle(false);
+            console.log("toggled modal to " + toggle);
+            modalVariables.id = null;
+            modalVariables.topic = null;
+            modalVariables.content = null;
+          }}
+          className="text-white text-xl w-fit px-1 rounded bg-red-600"
+        >
+          Cancel
+        </button>
+      </div>
+      <div className="mt-8 flex flex-wrap w-full">
+        <h1>Topic</h1>
+        <input
+          type="text"
+          placeholder="topic"
+          className="w-fit mb-4 text-3xl rounded-xl"
+          onChange={(e) => {
+            updatedTopic = e.target.value;
+          }}
+        />
+        <h1>Content</h1>
+        <input
+          type="text"
+          placeholder="content"
+          className="w-fit mb-4 text-3xl rounded-xl"
+          onChange={(e) => {
+            updatedContent = e.target.value;
+          }}
+        />
+      </div>
+      <button
+        onClick={async () => {
+          const cardDoc = doc(db, temp.currentUser.email, modalVariables.id);
+          updateDoc(cardDoc, { title: updatedTopic, content: updatedContent });
+          modalVariables.id = null;
+          modalVariables.topic = null;
+          modalVariables.content = null;
+          await getCardList();
+          setToggle(false);
+        }}
+        className="bg-blue-600 text-white mx-auto py-1 px-7"
+      >
+        Update
+      </button>
+    </div>
+  );
 
   return (
     <div className="w-screen h-fit p-0">
@@ -108,13 +206,13 @@ export function HomePage() {
                 deleteCard(card.id);
               }}
               updateFunction={() => {
-                updateCard(card.id, updatedTopic, updatedContent);
+                updateCard(card.id);
               }}
             ></Card>
           );
         })}
       </div>
-      <Modal toggle={toggle}></Modal>
+      <Modal toggle={toggle}>{modalContent}</Modal>
     </div>
   );
 }
