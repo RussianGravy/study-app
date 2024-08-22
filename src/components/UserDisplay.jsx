c0608b516188125d22393e635f8fcd6115a7e61f
RussianGravy <valentinejones1215@gmail.com>
2024-08-22 14:32:17 -0700

    add margin to bottom

--- src/components/UserDisplay.jsx
+++ src/components/UserDisplay.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../contexts/AuthContext";
 export function UserDisplay() {
   const temp = useAuth();
   return (
-    <div className="min-w-96 w-4/12 h-4/12 bg-gray-600 rounded-lg mx-auto">
+    <div className="min-w-96 w-4/12 h-4/12 bg-gray-600 rounded-lg mx-auto portrait:mb-7">
       <h2 className="text-white text-2xl m-8">{temp.currentUser.email}</h2>
     </div>
   );
