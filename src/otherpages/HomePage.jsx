a58ee79122f0d5509b405cf349f04c32975f822d
RussianGravy <valentinejones1215@gmail.com>
2024-08-22 11:21:38 -0700

    added shadow on left of deck box

--- src/otherpages/HomePage.jsx
+++ src/otherpages/HomePage.jsx
@@ -90,37 +90,44 @@ export function HomePage() {
   }
 
   const CreateDeck = (
-    <div className="bg-slate-300 flex flex-col mx-5 my-14 w-80 h-96 p-3 rounded-lg border-2 border-black relative overflow-hidden">
-      <div className="text-5xl h-7 w-full self-center flex z-30">
-        <p className="grow text-2xl">New Deck</p>
+    <div className="w-fit h-fit mx-5 my-14 p-3 relative hover:cursor-pointer">
+      <div
+        className={
+          "absolute top-3 bg-slate-300 w-80 h-96 rounded-lg border-2 border-black overflow-hidden -rotate-3"
+        }
+      ></div>
+      <div className="bg-slate-300 flex flex-col w-80 h-96 p-3 rounded-lg border-2 border-black relative overflow-hidden">
+        <div className="text-5xl h-7 w-full self-center flex z-30">
+          <p className="grow text-2xl">New Deck</p>
+          <button
+            onClick={async () => {
+              await setModalToggle(false);
+            }}
+            className="text-white text-xl w-fit px-1 rounded bg-red-600"
+          >
+            Cancel
+          </button>
+        </div>
+        <div className="flex flex-wrap w-full">
+          <input
+            type="text"
+            placeholder=" topic"
+            className="w-11/12 text-5xl rounded-xl mx-auto mt-10 mb-24"
+            onChange={(e) => {
+              setNewDeckName(e.target.value);
+            }}
+          ></input>
+        </div>
         <button
           onClick={async () => {
-            await setModalToggle(false);
+            createDeck(newDeckName);
+            setNewDeckName("");
           }}
-          className="text-white text-xl w-fit px-1 rounded bg-red-600"
+          className="bg-blue-600 text-white mx-auto py-1 w-10/12"
         >
-          Cancel
+          Create
         </button>
       </div>
-      <div className="flex flex-wrap w-full">
-        <input
-          type="text"
-          placeholder=" topic"
-          className="w-11/12 text-5xl rounded-xl mx-auto mt-10 mb-24"
-          onChange={(e) => {
-            setNewDeckName(e.target.value);
-          }}
-        ></input>
-      </div>
-      <button
-        onClick={async () => {
-          createDeck(newDeckName);
-          setNewDeckName("");
-        }}
-        className="bg-blue-600 text-white mx-auto py-1 w-10/12"
-      >
-        Create
-      </button>
     </div>
   ); //end of Create Deck;
 
@@ -135,24 +142,31 @@ export function HomePage() {
       ></CreateCardButton>
       <div className="pt-5 pb-10 w-screen flex flex-col">
         <h1 className="text-white text-5xl mt-20 ml-10">Your Decks</h1>
-        <div className="bg-gray-600 w-11/12 rounded-lg m-auto mt-5 px-8 h-max flex flex-row flex-nowrap overflow-x-scroll">
-          {decks.length > 0 ? (
-            decks.split(",").map((name) => {
-              return (
-                <Deck
-                  title={name}
-                  subject={""}
-                  selectFunction={() => {
-                    selectDeck(name);
-                  }}
-                  key={name}
-                ></Deck>
-              );
-            })
+        <div className="bg-gray-600 h-fit w-11/12 rounded-lg m-auto mt-5 relative">
+          <div className="w-full h-max px-8 flex flex-row flex-nowrap overflow-x-scroll">
+            {decks.length > 0 ? (
+              decks.split(",").map((name) => {
+                return (
+                  <Deck
+                    title={name}
+                    subject={""}
+                    selectFunction={() => {
+                      selectDeck(name);
+                    }}
+                    key={name}
+                  ></Deck>
+                );
+              })
+            ) : (
+              <h2 className="my-auto ml-6 py-2 text-4xl text-gray-300">
+                No decks to display. Press the 'plus' to begin your first deck.
+              </h2>
+            )}
+          </div>
+          {decks.split(",").length > 1 ? (
+            <div className="absolute top-0 right-0 h-full w-28 from-transparent to-gray-700 bg-gradient-to-r"></div>
           ) : (
-            <h2 className="my-auto ml-6 py-2 text-4xl text-gray-300">
-              No decks to display. Press the 'plus' to begin your first deck.
-            </h2>
+            ""
           )}
         </div>
         <h1 className="text-white text-5xl mt-10 ml-10">Friends</h1>
