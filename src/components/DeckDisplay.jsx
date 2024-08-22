627a858c1d3ff4a8e8d6c9ddad3575f30261a49b
RussianGravy <valentinejones1215@gmail.com>
2024-08-22 14:13:50 -0700

    changed decks input to string instead of array

--- src/components/DeckDisplay.jsx
+++ src/components/DeckDisplay.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Deck } from "./Deck";
 import { useDeck } from "../contexts/DeckContext";
 
-export function DeckDisplay({ decks, sf }) {
+export function DeckDisplay({ decks }) {
+  console.log(decks);
   const deckValues = useDeck();
   const navigate = useNavigate();
 
@@ -13,11 +14,11 @@ export function DeckDisplay({ decks, sf }) {
   }
 
   return (
-    <div className="bg-gray-600 h-fit w-7/12 portrait:w-11/12 rounded-lg relative mx-auto">
+    <div className="bg-gray-600 h-fit min-h-96 w-7/12 portrait:w-11/12 rounded-lg relative mx-auto">
       <h1 className="text-white text-5xl mt-5 ml-10">Your Decks</h1>
       <div className="w-full h-max px-8 flex flex-row flex-nowrap overflow-x-scroll">
         {decks.length > 0 ? (
-          decks.map((name) => {
+          decks.split(",").map((name) => {
             return (
               <Deck
                 title={name}
@@ -30,12 +31,12 @@ export function DeckDisplay({ decks, sf }) {
             );
           })
         ) : (
-          <h2 className="my-auto ml-6 py-2 text-4xl text-gray-300">
+          <h2 className="my-8 ml-8 py-2 text-4xl text-gray-300">
             No decks to display. Press the 'plus' to begin your first deck.
           </h2>
         )}
       </div>
-      {decks.length > 1 ? (
+      {decks.split(",").length > 1 ? (
         <div className="absolute top-0 right-0 h-full w-16 rounded-lg from-transparent to-gray-800 bg-gradient-to-r"></div>
       ) : (
         ""
