144f2672cb383767b8844575485b20cd23a951e8
RussianGravy <valentinejones1215@gmail.com>
2024-08-22 18:18:04 -0700

    changed name to 'The Social Study-App'

--- src/otherpages/LoginPage.jsx
+++ src/otherpages/LoginPage.jsx
@@ -17,7 +17,7 @@ export function LoginPage() {
     <div className="relative flex flex-row w-screen h-screen p-0 m-0 overflow-hidden">
       <div className="h-fit w-fit flex flex-row portrait:flex-col portrait:w-min self-center mx-auto">
         <h1 className="text-5xl w-fit landscape:mr-72 portrait:mb-6 portrait:text-center font-serif">
-          Personal Study App
+          The Social Study-App
         </h1>
         <div className="m-auto p-0">
           <Auth></Auth>
