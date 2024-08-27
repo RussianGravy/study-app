8dbdfa3a16ae9332e511cb51bbd292713ef45271
RussianGravy <valentinejones1215@gmail.com>
2024-08-27 09:42:02 -0700

    style changes

--- src/components/UserDisplay.jsx
+++ src/components/UserDisplay.jsx
@@ -26,7 +26,7 @@ export function UserDisplay() {
     <div className="min-w-96 w-4/12 h-4/12 bg-gray-600 rounded-lg mx-auto portrait:mb-7  flex flex-col">
       <div className="w-fit flex flex-row items-center m-5">
         <div className="h-full aspect-square rounded-full outline outline-gray-800 bg-green-500"></div>
-        <h2 className="text-white text-4xl ml-3">{username}</h2>
+        <h2 className="text-white text-4xl w-fit ml-3">{username}</h2>
       </div>
     </div>
   );
