5391144b92e48ea7359945b254025dacfea86334
RussianGravy <valentinejones1215@gmail.com>
2024-08-27 13:38:23 -0700

    added ProfilePage

---
+++ src/otherpages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+export function ProfilePage({ user }) {
+  return (
+    <div className="w-screen h-screen">
+      <Navbar />
+      ProfilePage
+    </div>
+  );
+}
