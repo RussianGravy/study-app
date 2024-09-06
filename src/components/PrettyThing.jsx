01fe3be686ba3786185cc9aaa4d9faebc0393df4
RussianGravy <valentinejones1215@gmail.com>
2024-09-06 00:19:09 -0700

    added lines to card design

--- src/components/PrettyThing.jsx
+++ src/components/PrettyThing.jsx
@@ -15,25 +15,29 @@ export function PrettyThing() {
             right: "22vw",
           }}
         >
-          <h1
-            className="mx-auto text-black text-7xl text-center font-serif"
-            style={{ fontSize: "5.5vw", marginTop: "7vw" }}
-          >
-            Make
-          </h1>
+          <div className="w-10/12 h-5/6 m-auto border-2 border-slate-300">
+            <h1
+              className="mx-auto text-black text-7xl text-center font-serif"
+              style={{ fontSize: "5.5vw", marginTop: "7vw" }}
+            >
+              Make
+            </h1>
+          </div>
         </div>
         <div
           className={
-            "bg-blue-300 rounded-lg border-2 border-black overflow-hidden"
+            "bg-blue-300 rounded-lg border-2 border-black overflow-hidden flex"
           }
           style={{ width: "24vw", aspectRatio: "80/96" }}
         >
-          <h1
-            className="mx-auto text-black text-7xl text-center font-serif "
-            style={{ fontSize: "5.5vw", marginTop: "7vw" }}
-          >
-            Share
-          </h1>
+          <div className="w-10/12 h-5/6 m-auto border-2 border-slate-300">
+            <h1
+              className="mx-auto text-black text-7xl text-center font-serif "
+              style={{ fontSize: "5.5vw", marginTop: "7vw" }}
+            >
+              Share
+            </h1>
+          </div>
         </div>
         <div
           className={
@@ -46,12 +50,14 @@ export function PrettyThing() {
             left: "22vw",
           }}
         >
-          <h1
-            className="mx-auto text-black text-7xl text-center font-serif"
-            style={{ fontSize: "5.5vw", marginTop: "7vw" }}
-          >
-            Study
-          </h1>
+          <div className="w-10/12 h-5/6 m-auto border-2 border-slate-300">
+            <h1
+              className="mx-auto text-black text-7xl text-center font-serif"
+              style={{ fontSize: "5.5vw", marginTop: "7vw" }}
+            >
+              Study
+            </h1>
+          </div>
         </div>
       </div>
     </>
