6aa11fbb41bb06014196f12341eb46e8ab1299a0
RussianGravy <valentinejones1215@gmail.com>
2024-09-09 00:02:54 -0700

    displays user name

--- src/otherpages/ProfilePage.jsx
+++ src/otherpages/ProfilePage.jsx
@@ -2,10 +2,28 @@ import React, { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { Navbar } from "../components/Navbar";
+import { Deck } from "../components/Deck";
+import { DeckDisplay } from "../components/DeckDisplay";
+import { useDeck } from "../contexts/DeckContext";
 import styles from "../components/custom_css/global.module.css";
 
-export function ProfilePage(userEmail) {
-  const [username, setUsername] = useState();
+export function ProfilePage() {
+  const [username, setUsername] = useState("");
+  const [decks, setDecks] = useState("");
+  const deckValues = useDeck();
+  const userEmail = deckValues.view;
+
+  useEffect(() => {
+    getUsername();
+    getDecks();
+  }, []); // getting initial values
+
+  useEffect(() => {
+    document.body.className = styles.homeBody;
+    return () => {
+      document.body.className = "";
+    };
+  }, []); // body styling for this page
 
   async function getUsername() {
     try {
@@ -17,21 +35,38 @@ export function ProfilePage(userEmail) {
     }
   }
 
-  useEffect(() => {
-    getUsername();
-  }, []); // getting initial values
-
-  useEffect(() => {
-    document.body.className = styles.homeBody;
-    return () => {
-      document.body.className = "";
-    };
-  }, []); // body styling for this page
+  async function getDecks() {
+    try {
+      const ref = doc(db, userEmail, "decks");
+      const names = (await getDoc(ref)).data().all_names;
+      setDecks(names);
+    } catch (err) {
+      console.error(err);
+      setDecks("");
+    }
+  }
 
   return (
-    <div className="w-screen h-screen flex">
+    <div className="w-screen h-screen flex flex-col">
       <Navbar currentPage={"Profile"} />{" "}
-      <div className="m-auto">{username}</div>
+      <div className="mt-28 ml-20 portrait:ml-5 flex flex-row flex-wrap">
+        <h1 className="text-blue-600 text-5xl portrait:text-3xl text-nowrap">
+          {username}
+        </h1>
+        <h1 className="text-5xl portrait:text-3xl ml-3 text-nowrap">
+          - Profile
+        </h1>
+      </div>
+      <div className="w-11/12 h-1 mt-5 mx-auto bg-gray-400 text-white">.</div>
+      <div className="w-10/12 mt-10 ml-20 portrait:ml-5 flex flex-row flex-wrap">
+        {decks.length > 0 ? (
+          <></> // <DeckDisplay decks={decks} show_name={false} />
+        ) : (
+          <h1 className="mt-5 ml-20 text-3xl">
+            This user has no decks to share. Come back later.
+          </h1>
+        )}
+      </div>
     </div>
   );
 }
