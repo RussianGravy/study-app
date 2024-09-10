3f85df1c44eae64d58bf0ff9673fca8919d4c88f
RussianGravy <valentinejones1215@gmail.com>
2024-09-09 17:10:40 -0700

    test for decks + added edit button

--- src/otherpages/ProfilePage.jsx
+++ src/otherpages/ProfilePage.jsx
@@ -1,25 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
+import { useAuth } from "../contexts/AuthContext";
 import { Navbar } from "../components/Navbar";
 import { Deck } from "../components/Deck";
 import { DeckDisplay } from "../components/DeckDisplay";
 import { useDeck } from "../contexts/DeckContext";
+import edit_icon from "../assets/edit_icon.png";
 import backgroundImage from "../assets/background.png";
 import styles from "../components/custom_css/global.module.css";
 
 export function ProfilePage() {
-  const [username, setUsername] = useState("");
+  const [userEmail, setUserEmail] = useState("");
   const [decks, setDecks] = useState("");
   const { id } = useParams();
+  const temp = useAuth();
 
   useEffect(() => {
     document.body.className = styles.homeBody;
     return () => {
       document.body.className = "";
     };
-  }, []);
+  }, []); // setting body styles
+
+  useEffect(() => {
+    getUserEmail();
+  }, []); // initializing user email
+
+  useEffect(() => {
+    getDecks();
+  }, [userEmail]); // initializing decks
+
+  async function getUserEmail() {
+    try {
+      const ref = doc(db, "global_data", "users");
+      const data = await (await getDoc(ref)).data()[id];
+      setUserEmail(data);
+      console.log(id + ", " + data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function getDecks() {
+    try {
+      const ref = doc(db, userEmail, "decks");
+      const names = (await getDoc(ref)).data().all_names;
+      setDecks(names);
+    } catch (err) {
+      console.error(err);
+      setDecks("");
+    }
+  }
 
   return (
     <div className="w-screen h-screen flex flex-col">
@@ -34,13 +67,28 @@ export function ProfilePage() {
         <h1 className="text-5xl portrait:text-3xl ml-3 text-nowrap">
           - Profile
         </h1>
+        {userEmail == temp.currentUser.email ? (
+          <button
+            className="w-9 h-9 portrait:w-7 portrait:h-7 aspect-square p-1 mt-1 portrait:mt-0 ml-4 portrait:ml-2 self-center bg-blue-600 rounded-md"
+            onClick={() => {
+              console.log("clicked");
+            }}
+          >
+            <img
+              src={edit_icon}
+              className="w-full h-full invert brightness-0"
+            />
+          </button>
+        ) : (
+          ""
+        )}
       </div>
       <div className="w-11/12 h-1 mt-5 mx-auto bg-gray-400 text-white">.</div>
       <div className="w-10/12 mt-10 ml-20 portrait:ml-5 flex flex-row flex-wrap">
         {decks.length > 0 ? (
           <></> // <DeckDisplay decks={decks} show_name={false} />
         ) : (
-          <h1 className="mt-5 ml-20 text-3xl">
+          <h1 className="w-full mt-5 portrait:text-center text-3xl">
             This user has no decks to share. Come back later.
           </h1>
         )}
